f549e39c3a9e62aa1a18567f8aafeb7773ba305d
AbdelrhmanArafa <arafa128@gmail.com>
2025-11-05 13:50:10 +0200

    fix: update vite base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/abdelrhman_arafa-s_portfolio/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
